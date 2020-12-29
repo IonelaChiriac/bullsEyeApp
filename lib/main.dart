e908aa3d330ff1886ac297376f2a041899d7a44d
Ionela Chiriac <ionela.s.chiriac@gmail.com>
2020-12-29 21:37:26 +0200

    alert commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BullsEyeApp());
+
+class BullsEyeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BullsEye',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: GamePage(title: 'BullsEye'),
+    );
+  }
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  bool _alertIsVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to my first app!',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+            ),
+            FlatButton(
+              child: Text('Hit Me!', style: TextStyle(color: Colors.blue)),
+              onPressed: () {
+                this._alertIsVisible = true;
+                _showAlert(context);
+                print("Button pressed! $_alertIsVisible");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    Widget okButton = FlatButton(
+      child: Text("Awesome!"),
+      onPressed: () {
+        Navigator.of(context).pop();
+        this._alertIsVisible = false;
+        print("Awesome pressed! $_alertIsVisible");
+      },
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Hello there!"),
+          content: Text("This is my first pop-up."),
+          actions: [
+            okButton,
+          ],
+          elevation: 5,
+        );
+      },
+    );
+  }
+}
